45cc367cc2aadbca515c377bd93eb51126b95a14
MZubairUmar <m.zubairumar.com@gmail.com>
2023-09-17 03:37:28 +0500

    Change Font Family Animation

--- script.js
+++ script.js
@@ -2,14 +2,64 @@
 //     el: document.querySelector('#main'),
 //     smooth: true
 // });
-function circleMouseFollower() {
+
+function circleMouseFollower(xscale,yscale) {
     window.addEventListener("mousemove", function(dets){
         console.log(dets.clientX, dets.clientY);
-        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
     })
 }
-circleMouseFollower();
 
+
+function FirstPageAnim() {
+  var tl=gsap.timeline();
+
+  tl.from("#nav",{
+    y:'-10',
+    opacity: 0,
+    duration:1.5,
+    ease:Expo.easeInOut
+  })
+
+  .to(".bondingelem",{
+    y:'0',
+    duration:2,
+    ease:Expo.easeInOut,
+    delay: -1,
+    stagger:.2
+  })
+
+  .from("#herofooter",{
+    y:'-10',
+    opacity: 0,
+    duration:1.5,
+    delay: -1,
+    ease:Expo.easeInOut
+  })
+}
+
+
+function circlestrecth(){
+
+  var xscale = 1;
+  var xyscale = 1;
+
+  var xprev = 0;
+  var yprev = 0;
+
+  window.addEventListener("mousemove", function(dets){
+    xscale=gsap.utils.clamp(.8,1.2,dets.clientX - xprev);
+    yscale=gsap.utils.clamp(.8,1.2,dets.clientY - yprev);
+
+    xprev=dets.clientX;
+    yprev=dets.clientY;
+
+    circleMouseFollower(xscale, yscale); 
+  })
+}
+circlestrecth();
+circleMouseFollower();
+FirstPageAnim();
 document.querySelectorAll(".elem").forEach(function (elem) {
     var rotate = 0;
     var diffrot = 0;
